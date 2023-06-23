a3ccf30f157a476f529563188ad0786ae747c010
asya-seag <asyaseagrave@gmail.com>
2023-06-23 21:07:25 +0100

    SearchBar

--- src/screens/MyPlants.js
+++ src/screens/MyPlants.js
@@ -1,20 +1,34 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { ImageBackground } from 'react-native';
+import { SearchBar } from '@rneui/themed';
+import React, { useState } from 'react';
 
 
 export default function MyPlants({ navigation }) {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSearch = (value) => {
+    setSearchValue(value);
+    // Search logic to be added later
+  };
+
   return (
     <ImageBackground
     source={require('../../assets/images/Plant_app_background.png')}
     style={styles.backgroundImage}
     imageStyle={styles.backgroundImageStyle}
     >
-      <View style={styles.container}>
-        <Text style={styles.textStyle}>My Plants</Text>
-        <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate('About')}>
-          <Text style={styles.textStyle}>Go back!</Text>
-        </TouchableOpacity>
-      </View>
+
+      <View style={styles.searchContainer}>
+          <SearchBar
+            platform="default"
+            placeholder="Type query here..."
+            onChangeText={handleSearch}
+            value={searchValue}
+            inputStyle={styles.searchInput}
+            containerStyle={styles.searchBarContainer}
+          />
+        </View>
     </ImageBackground>
   );
 }
@@ -63,5 +77,30 @@ const styles = StyleSheet.create({
     fontWeight: 'normal',
     letterSpacing: 0.25,
     color: 'black',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative', // Add position: 'relative' to the container
+  },
+  searchContainer: {
+    position: 'absolute', // Use absolute positioning for the search container
+    top: 0, // Position it at the top
+    width: '100%', // Take up the full width
+    alignItems: 'center', // Center the search bar horizontally
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  searchBarContainer: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    width: '90%', // Adjust the width as needed
+  },
+  searchInput: {
+    paddingVertical: 8,
   },
 });
