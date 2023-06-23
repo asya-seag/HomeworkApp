1bf2832593f71a5db97d6052f4f49a3a8ee1d087
asya-seag <asyaseagrave@gmail.com>
2023-06-23 10:25:50 +0100

    Page layout, tested background

---
+++ src/screens/MyPlants.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function MyPlants({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>My Plants</Text>
+      <TouchableOpacity style={styles.buttonStyle} onPress={() => navigation.navigate('Welcome')}>
+        <Text style={styles.textStyle}>Go back!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FEE440',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonStyle: {
+    backgroundColor: '#F62D14',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  textStyle: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'normal',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
+});
